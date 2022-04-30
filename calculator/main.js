657d7469f24259eded7de173496fa2b14a56f863
Guilherme Augusto <Guilhermelive980@hotmail.com>
2022-04-30 20:27:09 -0300

    changes

--- calculator/main.js
+++ calculator/main.js
@@ -14,34 +14,55 @@ keys.addEventListener('click', event => {
     const displayValue = display.textContent
     const {type} = key.dataset
     const {previousKeyType} = calculator.dataset
-    //Is this a Number key
+    
 
     if (type ==='number'){
 
-        if (displayValue === '0'){ 
+        if (displayValue === '0' || previousKeyType === 'operator'){ 
         display.textContent = keyValue
-        } else if (previousKeyType === 'operator') {
-
-            display.textContent = keyValue
-        }
-        
-        
+        } 
         else {
             display.textContent = displayValue + keyValue
         }
-
-
     }
 
-    //Is this a Operator key
+    
     
     if(type === 'operator') {
-        console.log(key)
+        const operatorKeys = keys.querySelectorAll('[data-type="operator"]')
+        operatorKeys.forEach(el => {el.dataset.state=''})       
+        key.dataset.state = 'selected'
 
-        calculator.dataset.previousKeyType = 'operator'
+        calculator.dataset.firstNumber = displayValue
+        calculator.dataset.operator = key.dataset.key
     }
 
+    if (type === 'equal') {
+        const firstNumber = calculator.dataset.firstNumber
+        const operator = calculator.dataset.operator
+        const secondNumber = displayValue
+
+        display.textContent = calculate(firstNumber, operator, secondNumber)
+    }
+
+    if (type === 'clear') {
+
+    }
 
     calculator.dataset.previousKeyType = type
 
 })
+
+function calculate (firstNumber, operator, secondNumber) {
+    firstNumber = parseFloat(firstNumber)
+    secondNumber = parseFloat(secondNumber)
+    
+    if (operator === 'plus') return  firstNumber + secondNumber
+    if (operator === 'minus') return  firstNumber - secondNumber
+    if (operator === 'times')  return firstNumber * secondNumber
+    if (operator === 'divide') return firstNumber / secondNumber
+
+    
+
+
+}
